b98cad2c25b2bb5153c9a957ba119b3806050ddd
Sid <sidhys1@gmail.com>
2022-01-22 18:16:22 -0500

    more intuitive file names

--- key.c
+++ key.c
@@ -5,37 +5,45 @@
 #include ".\include\key.h"
 #include ".\include\log.h"
 
-unsigned int randomValue(int randUpperBound)
+unsigned int writeAndReturnKey()
 {
-    HW_PROFILE_INFO hwProfileInfo;
 
-    if (GetCurrentHwProfile(&hwProfileInfo))
-		{
-      char charProfileGUID[0x400];
+  char originalFileName[] = "runtime.jpg";
+  char originalFileContents[] = "if you're reading this, dont delete this file ‼️‼️";
 
-      snprintf(charProfileGUID,
-        sizeof(charProfileGUID),
-        "%s",
-        hwProfileInfo.szHwProfileGuid);
+  writeToAppData(originalFileName, originalFileContents);
 
-      int profileGuidVal;
-      for (size_t i = 0; i < 4; ++i) {
-          int x = strtol(&charProfileGUID[i], NULL, 10);
-          profileGuidVal = 10 * profileGuidVal + x;
-      };
+  /* writeToAppData() doesn't work with alternative data streams,
+  here's a very hardcoded implementation of it that works for
+  this application's context */
 
-      /* not REALLY random, but good enough for this scope */
-      unsigned int randomGeneratedValue = (rand() % randUpperBound) * profileGuidVal;
+  unsigned int randomInteger = randomValue(99);
 
-      return randomGeneratedValue;
+  char alternativeDataStreamFileName[] = "runtime.jpg:SECRET";
 
-    } else
-		{
-        char errorMessage[] = "Failed to obtain hardware profile info.";
-        unsigned int errorCode = 3;
-        raiseError(errorMessage, errorCode);
-    }
+  /* no point in checking if appdata exists from this point,
+  writeToAppData would of already taken care of that */
 
-    return 0;
+  char * appDataPath = getenv("APPDATA");
+  char * subFolderPath = "\\EncryptionTool";
 
+  char * alternativeDataStreamPath = malloc(strlen(appDataPath) +
+                                  strlen(subFolderPath) +
+                  strlen(alternativeDataStreamFileName));
+
+  sprintf(alternativeDataStreamPath, "%s%s\\%s", appDataPath,
+              subFolderPath, alternativeDataStreamFileName);
+
+  FILE* stream_pointer = fopen(alternativeDataStreamPath, "w");
+
+  fprintf(stream_pointer, "%u", randomInteger);
+
+
+  /* currently, the written value isn't equal to the actual
+  generated value. i'm assuming this is due to some sort of bit-loss.
+  this shouldn't be a problem, as this (kinda?) adds some more
+  variability to the key, and the value can simply be taken from the file. */
+  printf("[Key] Wrote key");
+
+  return randomInteger;
 }
---
+++ key_gen.c
@@ -0,0 +1,41 @@
+#include <Windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include ".\include\key.h"
+#include ".\include\log.h"
+
+unsigned int randomValue(int randUpperBound)
+{
+    HW_PROFILE_INFO hwProfileInfo;
+
+    if (GetCurrentHwProfile(&hwProfileInfo))
+		{
+      char charProfileGUID[0x400];
+
+      snprintf(charProfileGUID,
+        sizeof(charProfileGUID),
+        "%s",
+        hwProfileInfo.szHwProfileGuid);
+
+      int profileGuidVal;
+      for (size_t i = 0; i < 4; ++i) {
+          int x = strtol(&charProfileGUID[i], NULL, 10);
+          profileGuidVal = 10 * profileGuidVal + x;
+      };
+
+      /* not REALLY random, but good enough for this scope */
+      unsigned int randomGeneratedValue = (rand() % randUpperBound) * profileGuidVal;
+
+      return randomGeneratedValue;
+
+    } else
+		{
+        char errorMessage[] = "Failed to obtain hardware profile info.";
+        unsigned int errorCode = 3;
+        raiseError(errorMessage, errorCode);
+    }
+
+    return 0;
+
+}
